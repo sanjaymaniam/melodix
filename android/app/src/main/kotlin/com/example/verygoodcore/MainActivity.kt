20416e50ed0332eb4cfcc36488b523d400967cc9
Sanjay Subramaniam <sanjaysubramaniam99@gmail.com>
2024-06-08 12:50:38 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.melodix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:melodix/app/app.dart';
+import 'package:melodix/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
